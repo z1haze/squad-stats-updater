d6654a19be698d9f0f459ac5484f738ca19cc0e3
z1haze <stephen.hendricks87@gmail.com>
2023-12-10 17:08:29 -0700

    add basic logging for the redis client connection

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -1,8 +1,25 @@
 import Redis from "ioredis";
 import env from "../util/env";
 
-export default new Redis({
+const redis = new Redis({
   host: env.REDIS_HOST,
   port: env.REDIS_PORT,
   password: env.REDIS_PASS
 });
+
+// Listening to the 'connect' event
+redis.on('connect', () => {
+  console.log('Connected to Redis');
+});
+
+// Listening to the 'ready' event
+redis.on('ready', () => {
+  console.log('Redis is ready to receive commands');
+});
+
+// Handle connection error
+redis.on('error', (err) => {
+  console.error('Error connecting to Redis', err);
+});
+
+export default redis;
